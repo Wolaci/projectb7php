65720a9b5840a975d9e25fc9b86011b66ff82ae8
Wolaci <wolacijose8@gmail.com>
2023-01-16 18:50:27 -0300

    Finalizando exibição de usuários

--- userscontrol/index.php
+++ userscontrol/index.php
@@ -13,15 +13,20 @@
     <table border="0" width="100%">
         <tr>
             <th>Nome</th>
-            <th>E-mail</th>
             <th>Idade</th>
+            <th>Ações</th>
         </tr>
         <?php
             $sql = "SELECT * FROM usuarios";
             $sql = $pdo->query($sql);
             if($sql->rowCount()>0){
                 foreach($sql->fetchAll() as $usuario){
-                    
+                    echo '<tr>';
+                    echo '<td>'.$usuario['nome'].'</td>';
+                    echo '<td>'.$usuario['idade'].'</td>';
+                    echo '<td><a href="editar.php?id='.$usuario['id'].'">Editar</a></td>';        
+                    echo '<td><a href="excluir.php?id='.$usuario['id'].'">Excluir</a></td>';
+                    echo '</tr>';
                 }
             }
         ?>
