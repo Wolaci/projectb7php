c55fcc6e51d60b949d458febf2baf562c70f1cbd
Wolaci <wolacijose8@gmail.com>
2023-02-28 18:02:46 -0300

    Iniciando read na prática

--- contactcrud/index.php
+++ contactcrud/index.php
@@ -2,3 +2,16 @@
 
 include 'contato.class.php';
 $contato = new Contato();
+
+?>
+
+<h1>Contatos</h1>
+
+<table border="1" width="600">
+    <tr>
+        <th>ID</th>
+        <th>NOME</th>
+        <th>E-MAIL</th>
+        <th>AÇÕES</th>
+    </tr>
+</table>
